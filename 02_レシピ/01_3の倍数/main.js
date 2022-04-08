776f6b6ff3faf1e055a01276eb64a88836d3ba1a
Tatsuki-Kurosawa <kuro96831@gmail.com>
2022-04-09 01:47:25 +0900

    FizzBuzz

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,21 +1,15 @@
-// const genkiFunction = funstion(number) {
-//     for (let n = 1; n <= number; n++) {
-//         if (number % 3 == 0) {
-//           console.log("${n}!!!!!!!")
-//         } else {
-//           console.log(n)
-//         }
-//       }
-// }
-
-const genkiFunction = function (number) {
-  for (let n = 1; n <= number; n++) {
-    if (n % 3 === 0) {
-      console.log(`${n}!!!!!!!`)
+const FizzBuzz = function (number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 15 == 0) {
+      console.log("FizzBuzz")
+    } else if (i % 3 == 0) {
+      console.log("Fizz")
+    } else if (i % 5 == 0) {
+      console.log("Buzz")
     } else {
-      console.log(n)
+      console.log(i)
     }
   }
 }
 
-genkiFunction(100)
+FizzBuzz(15)

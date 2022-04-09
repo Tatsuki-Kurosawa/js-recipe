cb725c9c3c1592640f522a64146327fce4ff925d
Tatsuki-Kurosawa <kuro96831@gmail.com>
2022-04-09 16:35:35 +0900

    dentaku

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,21 +1,32 @@
 const display = document.getElementById("display")
-const plusButton = document.getElementById("plus-button")
-const minusButton = document.getElementById("minus-button")
-const twiceButton = document.getElementById("twice-button")
+const number1Box = document.getElementById("number1")
+const number2Box = document.getElementById("number2")
+const sign = document.getElementById("sign-button")
 
-let count = 0
+let result
 
-plusButton.onclick = function () {
-  count++
-  display.textContent = count
+const Plus = sign.children[0]
+const Minus = sign.children[1]
+const Mul = sign.children[2]
+const Div = sign.children[3]
+const Equal = sign.children[4]
+
+Plus.onclick = function () {
+  result = parseInt(number1Box.value) + parseInt(number2Box.value)
 }
 
-minusButton.onclick = function () {
-  count--
-  display.textContent = count
+Minus.onclick = function () {
+  result = number1Box.value - number2Box.value
 }
 
-twiceButton.onclick = function () {
-  count *= 2
-  display.textContent = count
+Mul.onclick = function () {
+  result = number1Box.value * number2Box.value
+}
+
+Div.onclick = function () {
+  result = number1Box.value / number2Box.value
+}
+
+Equal.onclick = function () {
+  display.textContent = result
 }

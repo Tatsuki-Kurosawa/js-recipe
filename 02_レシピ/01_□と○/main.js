c8d3064e99948990a7a0539dcb7b408e930f9887
Tatsuki-Kurosawa <kuro96831@gmail.com>
2022-04-10 13:17:29 +0900

    [□と○]発展課題2つ目を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,5 +1,20 @@
-const figure_1 = document.getElementById("figure_1")
+const figure = document.getElementById("figure")
 
-figure_1.onmouseenter = function () {
-  figure_1.classList.toggle("rounded")
+let i = 0
+
+figure.onclick = function () {
+  if (i == 0) {
+    figure.classList.add("rounded")
+    i += 1
+  } else if (i == 1) {
+    figure.classList.add("triangle")
+    figure.classList.remove("rounded")
+    figure.classList.remove("square")
+    i += 1
+  } else {
+    figure.classList.add("square")
+    figure.classList.remove("rounded")
+    figure.classList.remove("triangle")
+  }
+  // remove("rounded square")というように複数指定するのではなく、1つずつ命令を指定することでうまくいった
 }

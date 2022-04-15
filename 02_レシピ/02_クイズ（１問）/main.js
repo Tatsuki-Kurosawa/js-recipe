282bd19ed4f638e3e536ffbf7e779d670149ae99
Tatsuki-Kurosawa <kuro96831@gmail.com>
2022-04-15 13:21:23 +0900

    [クイズ (1問)()]を学習しました。

--- 02_レシピ/02_クイズ（１問）/main.js
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -1,14 +1,17 @@
 const quizText = document.getElementById("quiz-text")
 const quizImage = document.getElementById("quiz-image")
-const choice1 = document.getElementById("choice-1")
-const choice2 = document.getElementById("choice-2")
-const choice3 = document.getElementById("choice-3")
-const choice4 = document.getElementById("choice-4")
+// const choice1 = document.getElementById("choice-1")
+// const choice2 = document.getElementById("choice-2")
+// const choice3 = document.getElementById("choice-3")
+// const choice4 = document.getElementById("choice-4")
 const feedback = document.getElementById("feedback")
+const choices = document.getElementById("choices-container")
+
+let i = 0
 
 // クイズの内容    オブジェクト
 const quiz = {
-  text: "この星の名前は何でしょう？",
+  text: "この城の名前は何でしょう？",
   image: "maruoka.png",
   choices: [
     {
@@ -28,6 +31,7 @@ const quiz = {
       feedback: "正解！北陸唯一の現存十二天守の一つだよ！",
     },
   ],
+  number: 4,
 }
 
 // quiz を画面に表示する関数
@@ -39,35 +43,35 @@ const reloadQuiz = function () {
   quizImage.src = "./images/" + quiz.image
 
   // 選択肢（ボタン）の中身を表示
-  choice1.textContent = quiz.choices[0].text
-  choice2.textContent = quiz.choices[1].text
-  choice3.textContent = quiz.choices[2].text
-  choice4.textContent = quiz.choices[3].text
+  while (i < quiz.number) {
+    button_tag(i)
+    click_tag(i)
+    i++
+  }
 }
 
-// choiceNumber番目の選択肢を選択
+const choice_array = []
+
+// 取得した要素の中にbuttonタグを作る？そもそもそういう使い方をしない？
+
+const button_tag = function (i) {
+  // button要素の作成
+  const choice_answer = document.createElement("button")
+  choice_answer.id = "choice" + String(i)
+  choice_answer.textContent = quiz.choices[i].text
+  choices.appendChild(choice_answer)
+  // 要素の取得
+  choice_array[i] = document.getElementById("choice" + String(i))
+}
+
+const click_tag = function (i) {
+  choice_array[i].onclick = function () {
+    choose(i)
+  }
+}
+
 const choose = function (choiceNumber) {
-  // フィードバックを表示
   feedback.textContent = quiz.choices[choiceNumber].feedback
 }
 
-choice1.onclick = function () {
-  // 0 番目の選択肢を選択
-  choose(0)
-}
-choice2.onclick = function () {
-  // 1 番目の選択肢を選択
-  choose(1)
-}
-choice3.onclick = function () {
-  // 2 番目の選択肢を選択
-  choose(2)
-}
-
-choice4.onclick = function () {
-  // 3 番目の選択肢を選択
-  choose(3)
-}
-
-// reloadQuiz関数 を実行して、クイズを画面に表示する
 reloadQuiz()
